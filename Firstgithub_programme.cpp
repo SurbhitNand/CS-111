84b4c52d5e76672498cdb1497ac82e9853faa271
SurbhitNand <surbhitnand@email.com>
2024-09-06 11:09:10 +1200

    Second committment

--- Firstgithub_programme.cpp
+++ Firstgithub_programme.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
 
+ int sum(int x,int y);
+
 int main() {
     int a = 5;
     int b = 10;
+    int total = 0;
 
-    int sum = a + b;
+	total = sum(a,b);
     
 	cout << "Hello world to my life " << endl;
-    cout << "The sum is: " << sum << endl;
+    cout << "The sum is: " << total << endl;
 
     return 0;
 }
-
+ int sum(int x,int y){
+ 	int sum = 0;
+ 	sum = x + y;
+ 	return sum;
+ }
